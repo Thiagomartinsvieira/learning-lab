a7e27881c47cfb6f55629bb51c4483eeed0eb52d
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-11-16 21:28:35 -0300

    feat: running app

---
+++ react-native/links/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Hello React Native!</Text>
+      <Text>My App</Text>
+    </View>
+  );
+};
+
+export default Index;
