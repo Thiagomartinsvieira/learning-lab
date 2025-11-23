b1f251f78b70d9243b3e5c562702904a37a8c6c0
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-11-23 15:19:56 -0300

    feat: creating the category component

--- react-native/links/src/app/index/index.tsx
+++ react-native/links/src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { Category } from "@/components/category";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
@@ -13,6 +14,8 @@ const Index = () => {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category />
     </View>
   );
 };
