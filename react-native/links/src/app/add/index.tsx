13f78ed1dbfe4f31d1a5dc374e5d5053aa9557c4
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-12-05 22:57:04 -0300

    feat: redirect user after create new link

--- react-native/links/src/app/add/index.tsx
+++ react-native/links/src/app/add/index.tsx
@@ -34,6 +34,10 @@ export default function Add() {
         url,
         category,
       });
+
+      Alert.alert("Sucesso", "Novo link adicionado", [
+        { text: "Ok", onPress: () => router.back() },
+      ]);
     } catch (error) {
       Alert.alert("Error", "Do not possible save the link");
       console.error(error);
