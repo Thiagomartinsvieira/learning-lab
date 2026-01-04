64f0995a258f6506d6ae5a54e2f2f1772e0fb53f
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-01-04 19:30:51 -0300

    feat: the authentication state persists

--- react-native/protected-routes/src/app/(protected)/_layout.tsx
+++ react-native/protected-routes/src/app/(protected)/_layout.tsx
@@ -1,8 +1,17 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Redirect, Stack } from "expo-router";
+import { ActivityIndicator } from "react-native";
 
 export default function ProtectedLayout() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isReady } = useAuth();
+
+  if (!isReady) {
+    return (
+      <ActivityIndicator
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      />
+    );
+  }
 
   if (!isLoggedIn) {
     return <Redirect href="/signIn" />;
--- react-native/protected-routes/src/contexts/authContext.tsx
+++ react-native/protected-routes/src/contexts/authContext.tsx
@@ -1,29 +1,64 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
 
 type AuthState = {
   isLoggedIn: boolean;
+  isReady: boolean;
   signIn: () => void;
   signOut: () => void;
 };
+
+const AUTH_STORAGE_KEY = "@protected-routes:auth-state";
 
 export const AuthContext = createContext<AuthState>({} as AuthState);
 
 export function AuthProvider({ children }: PropsWithChildren) {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isReady, setIsReady] = useState(false);
+
+  async function storageState(newState: { isLoggedIn: boolean }) {
+    try {
+      await AsyncStorage.setItem(AUTH_STORAGE_KEY, JSON.stringify(newState));
+    } catch (error) {
+      console.error("ERROR_SET_STORAGE_AUTH: ", error);
+    }
+  }
 
   const signIn = () => {
     setIsLoggedIn(true);
+    storageState({ isLoggedIn: true });
     router.replace("/");
   };
 
   const signOut = () => {
     setIsLoggedIn(false);
+    storageState({ isLoggedIn: false });
     router.replace("/signIn");
   };
 
+  useEffect(() => {
+    async function loadStorageState() {
+      try {
+        // Simulating API delay
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const storageState = await AsyncStorage.getItem(AUTH_STORAGE_KEY);
+        const state = storageState ? JSON.parse(storageState) : null;
+        console.log("STORAGE => ", state);
+        setIsLoggedIn(state?.isLoggedIn ?? false);
+      } catch (error) {
+        console.error("ERROR_GET_STORAGE_AUTH: ", error);
+        setIsReady(false);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    loadStorageState();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, signIn, signOut }}>
+    <AuthContext.Provider value={{ isLoggedIn, signIn, signOut, isReady }}>
       {children}
     </AuthContext.Provider>
   );
